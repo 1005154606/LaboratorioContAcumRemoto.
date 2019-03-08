3cac89039458ae9567007497e655751c9b0fd876
Escobar Silva <000404669@bga.upb>
2019-03-08 12:32:51 -0500

    Mi primer commit

---
+++ primos.py
@@ -0,0 +1,6 @@
+a=int(input("Ingrese un número: "))
+b=int(input("Ingrese un número: "))
+if a%b==0:
+    print("el número", a," es divisible exacto entre", b )
+else:
+    print("el número", a," no es divisible exacto entre", b )
