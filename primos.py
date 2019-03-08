9d9d1da1508911fefa75b782b91264c4fca5c9fc
Escobar Silva <000404669@bga.upb>
2019-03-08 13:46:33 -0500

    ejercicio resuelto

--- primos.py
+++ primos.py
@@ -1,4 +1,19 @@
 a=int(input("Ingrese un número: "))
+c=0
 for i in range(1, a):
     b=a%i
-    print(b)
+    if b==0:
+        c=c+1
+        if c==2:
+            print("El número no es primo")
+
+
+    else:
+        if c==1:
+            print("el número es primo")
+            break
+
+
+
+
+
