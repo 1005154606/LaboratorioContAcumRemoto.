abac9684b09c04266e8c079d81e169779e2a49c6
Escobar Silva <000404669@bga.upb>
2019-03-08 12:46:26 -0500

    Ya casi calculo un primo

--- primos.py
+++ primos.py
@@ -1,6 +1,4 @@
 a=int(input("Ingrese un número: "))
-b=int(input("Ingrese un número: "))
-if a%b==0:
-    print("el número", a," es divisible exacto entre", b )
-else:
-    print("el número", a," no es divisible exacto entre", b )
+for i in range(1, a):
+    b=a%i
+    print(b)
